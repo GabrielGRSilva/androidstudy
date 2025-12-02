5f37b1bb0017e2adf04eab784868632b8e4c3966
Gabriel Silva <gabrielgrsilva@outlook.com.br>
2025-12-02 14:10:15 -0300

    Used ArrayList to create new TextViews

--- app/src/main/java/com/example/aluramobile/MainActivity.java
+++ app/src/main/java/com/example/aluramobile/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.aluramobile;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import androidx.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> alunos = new ArrayList<>(Arrays.asList("AlunoDoido", "AlunoMaluco", "AlunoAlucicrazy"));
+        TextView aluno1 = findViewById(R.id.textView); //Cada textView usa o ID criado lá no res/layout
+        TextView aluno2 = findViewById(R.id.textView2);
+        TextView aluno3 = findViewById(R.id.textView3);
+        aluno1.setText(alunos.get(0)); //Pega o índice x da lista para atribuir àquele aluno
+        aluno1.setText(alunos.get(1));
+        aluno1.setText(alunos.get(2));
     }
 }
