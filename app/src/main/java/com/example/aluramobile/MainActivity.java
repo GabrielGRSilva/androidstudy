a33eec0702d9e1c71f55e3d4a9c6c5625749889f
Gabriel Silva <gabrielgrsilva@outlook.com.br>
2025-12-02 09:04:44 -0300

    First commit with code - Toast

---
+++ app/src/main/java/com/example/aluramobile/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.aluramobile;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Learning", Toast.LENGTH_LONG).show();
+    }
+}
